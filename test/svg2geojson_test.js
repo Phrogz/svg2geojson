37e2fc1a085c9ea2837ac2b05467ff4ec083efaa
Gemeng Qin <mail@gemengq.me>
2017-11-08 14:05:27 +0800

    Refactor for test case

--- test/svg2geojson_test.js
+++ test/svg2geojson_test.js
@@ -7,15 +7,34 @@ describe('svg2geojson', () => {
 
     it('should return geojson', (done) => {
       svg2geojson.geoFromSVGXML(
-        '<svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg"><MetaInfo xmlns="http://www.prognoz.ru"><Geo><GeoItem X="0" Y="0" Latitude="0.0" Longitude="0.0"/><GeoItem X="10" Y="10" Latitude="10.0" Longitude="10.0"/></Geo></MetaInfo><g><rect stroke="#000000" id="svg_1" height="10" width="10" y="0" x="0" stroke-width="5" fill="#FF0000"/></g></svg>',
+        '<svg width="10" height="10" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">' +
+          '<MetaInfo xmlns="http://www.prognoz.ru">' +
+            '<Geo>' +
+              '<GeoItem X="0" Y="0" Latitude="100.0" Longitude="100.0"/>' +
+              '<GeoItem X="10" Y="10" Latitude="200.0" Longitude="200.0"/>' +
+            '</Geo>' +
+          '</MetaInfo>' +
+          '<g>' +
+        '<rect stroke="#000000" id="svg_1" height="10" width="10" y="0" x="0" stroke-width="5" fill="#FF0000"/>' +
+        '</g>' +
+        '</svg>',
         (geojson) => {
-          var result = JSON.stringify(geojson)
-          expect(result).to.equal('{"type":"FeatureCollection","creator":"svg2geojson v0.7.0","features":[{"type":"Feature","properties":null,"geometry":{"type":"Polygon","coordinates":[[[0,0],[0,10],[10,10],[10,0],[0,0]]]}}]}')
+          let expected = {
+            type: 'FeatureCollection',
+            creator: 'svg2geojson v0.7.0',
+            features: [{
+              type: 'Feature',
+              properties: null,
+              geometry: {
+                type: 'Polygon',
+                'coordinates': [[[100, 100], [100, 200], [200, 200], [200, 100], [100, 100]]]
+              }
+            }]
+          };
+          expect(geojson).to.deep.equal(expected);
           done();
         }
       );
     });
-    
   });
-
 });
